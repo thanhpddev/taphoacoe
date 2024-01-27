5f19eb1161d5eb671eacddd850648980a3f07323
thanhpddev <thanhpddev@gmail.com>
2024-01-27 10:54:19 +0700

    3

--- js/index.js
+++ js/index.js
@@ -2,17 +2,21 @@ let searchBox = document.querySelector("#search-box");
 let images = document.querySelectorAll(".container .image-container .image");
 
 searchBox.oninput = () => {
+  $(".container p.clear").addClass("active");
   images.forEach((hide) => (hide.style.display = "none"));
-  let value = searchBox.value;
-  // let value = searchBox.value
-  //   .normalize("NFD")
-  //   .replace(/[\u0300-\u036f]/g, "")
-  //   .replace(/đ/g, "d")
-  //   .replace(/Đ/g, "D");
-  console.log(value);
-  images.forEach((filter) => {
-    let title = filter.getAttribute("data-title");
+  let value = searchBox.value
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/đ/g, "d")
+    .replace(/Đ/g, "D");
 
+  images.forEach((filter) => {
+    let title = filter
+      .getAttribute("data-title")
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .replace(/đ/g, "d")
+      .replace(/Đ/g, "D");
     if (
       value
         .toLowerCase()
@@ -26,3 +30,18 @@ searchBox.oninput = () => {
     }
   });
 };
+
+//clear input
+$("body").on("input", "#search-box", function () {
+  if ($("#search-box").val()) {
+    $(".container p.clear").addClass("active");
+  } else {
+    $(".container p.clear").removeClass("active");
+  }
+});
+
+$("body").on("click", "p.clear", function () {
+  $("#search-box").val("");
+  $(".container p.clear").removeClass("active");
+  $(".container .image-container > div").css({ display: "block" });
+});
