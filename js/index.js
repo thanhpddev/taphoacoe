efacca8bdcf77b11a7fc7b98ffa37eed67e03423
thanhpddev <thanhpddev@gmail.com>
2024-01-27 09:22:20 +0700

    2

--- js/index.js
+++ js/index.js
@@ -3,7 +3,12 @@ let images = document.querySelectorAll(".container .image-container .image");
 
 searchBox.oninput = () => {
   images.forEach((hide) => (hide.style.display = "none"));
-  let value = searchBox.value;
+  let value = searchBox.value
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/đ/g, "d")
+    .replace(/Đ/g, "D");
+  console.log(value);
   images.forEach((filter) => {
     let title = filter.getAttribute("data-title");
 
